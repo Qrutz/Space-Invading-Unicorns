eca3c005db1c25bd17cf7c79311786d5c1a86a05
Alexander <helsinga@student.chalmers.se>
2024-10-05 15:53:24 +0200

    Get city and country

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,15 @@
 from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+from urllib.request import urlopen
+import json
 
 app = FastAPI()
 
+class UserInput(BaseModel):
+    temperature: int
+    population: int
+    treeCoverage: int
 
 @app.get("/")
 def read_root():
@@ -13,3 +19,32 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/calculate")
+def getUserParametersAndCalculate(input_data: UserInput):
+    userAnswer = getplace(57.721035, 12.939819)
+    if userAnswer:
+        town, country = userAnswer
+        return {
+            "City": town,
+            "Country": country,
+        }
+    else:
+        return {"error": "Location not found"}
+
+def getplace(lat, lon):
+    url = f"http://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lon}&sensor=false"
+    try:
+        v = urlopen(url).read()
+        j = json.loads(v)
+        components = j['results'][0]['address_components']
+        country = town = None
+        for c in components:
+            if "country" in c['types']:
+                country = c['long_name']
+            if "postal_town" in c['types']:
+                town = c['long_name']
+        return town, country
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return None

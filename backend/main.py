e0493fbb604076997c8fa1b80c6488166c0695cc
Alexander <helsinga@student.chalmers.se>
2024-10-06 14:55:03 +0200

    Add parameters to fetch

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,13 @@ def read_root():
 @app.post("/calculate")
 def getUserParametersAndCalculate(userInput: UserInput):
     # Do ML model algo thingy
+
+    # Hardcoded data for example
     longitude, latitude = 57.721035, 12.939819
+    treeCoverage = 64
+    population = 112345
+    pollution = 15
+    temperature = 18
 
     # Request to Google Maps API to get country and city
     print(userInput)
@@ -36,6 +42,10 @@ def getUserParametersAndCalculate(userInput: UserInput):
             "Country": country,
             "Latitude": latitude,
             "Longitude": longitude,
+            "TreeCoverage": treeCoverage,
+            "Population": population,
+            "Temperature": temperature,
+            "Pollution": pollution
         }
     else:
         return {"error": "Location not found"}
